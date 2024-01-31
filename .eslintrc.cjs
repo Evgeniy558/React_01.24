0778b5973ae54aa4b95a37ae16da846fe65e7032
Evgenij Prishchepov <prishchepovee@gmail.com>
2024-01-31 21:57:55 +0300

    add rules: {prettier/prettier: error}

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off'
